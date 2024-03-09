0e96903f415aad577dd7921e1edbd5a6232dcef5
vkartk <53650724+vkartk@users.noreply.github.com>
2024-03-09 20:01:55 +0530

    Add tailwind

--- civic/settings.py
+++ civic/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'civic',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'civic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Compressor settings
+COMPRESS_ENABLED = True
+COMPRESS_ROOT = BASE_DIR / 'static'
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)

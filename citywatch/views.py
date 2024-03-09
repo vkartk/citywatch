204ad8396533d361bc60c81b30789f4d9a9d0fec
vkartk <53650724+vkartk@users.noreply.github.com>
2024-03-09 20:14:40 +0530

    Structure Pages

--- citywatch/views.py
+++ citywatch/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'pages/home.html')
 
 def map(request):
-    return render(request, 'map.html')
+    return render(request, 'pages/map.html')
 
 def reports(request):
-    return render(request, 'reports.html')
+    return render(request, 'pages/reports.html')
 
 def report(request):
-    return render(request, 'report.html')
+    return render(request, 'pages/report.html')

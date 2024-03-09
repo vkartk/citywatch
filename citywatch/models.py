09d8b3a72c33b590f2b4f2e8eeab5cf0e729ee5c
vkartk <53650724+vkartk@users.noreply.github.com>
2024-03-09 20:51:25 +0530

    Update Issue

--- citywatch/models.py
+++ citywatch/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Issue(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+
+    image = models.ImageField(blank=True, null=True, upload_to='images/issue/')
+    category = models.ForeignKey('IssueCategory', on_delete=models.CASCADE)
+
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class IssueCategory(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class IssueComment(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
--- citywatch/views.py
+++ citywatch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from .forms import IssueForm
 
 def home(request):
     return render(request, 'pages/home.html')
@@ -12,4 +13,14 @@ def reports(request):
     return render(request, 'pages/reports.html')
 
 def report(request):
-    return render(request, 'pages/report.html')
+    if request.method == "POST":
+        form = IssueForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Issue reported successfully!")
+        else:
+            messages.error(request, "Issue report failed!")
+    else:
+        form = IssueForm()
+
+    return render(request, "pages/report.html", {"form": form})

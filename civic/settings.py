f07175345b78cef6a67885b0b58762a22b98740c
vkartk <53650724+vkartk@users.noreply.github.com>
2024-08-27 17:14:47 +0530

    Migrate:  postgresql -> mysql

--- civic/settings.py
+++ civic/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'civic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT', default=5432),
+        'PORT': env('DB_PORT', default=3306),
     }
 }
 

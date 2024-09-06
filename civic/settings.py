085dda868480e8c4d16e06de383a4df0b8553fb4
vkartk <53650724+vkartk@users.noreply.github.com>
2024-09-06 11:05:07 +0530

    Add: STATICFILES_DIRS

--- civic/settings.py
+++ civic/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

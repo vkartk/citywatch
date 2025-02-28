ece88066b97908264bf9e4e52dde5586e016ac31
Karthik V <53650724+vkartk@users.noreply.github.com>
2025-02-28 23:10:19 +0530

    feat: use env vars for SECRET KEY

--- civic/settings.py
+++ civic/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env( os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='django-insecure-bh-1mre+tw=*^h+c^)=f3t81vh1#%&2wvk2djudk0j(b=p%fvj')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)

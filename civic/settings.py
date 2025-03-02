5e53df8c48a994e3f5191835da16ca191f4aa03f
vkartk <53650724+vkartk@users.noreply.github.com>
2025-03-02 15:45:36 +0530

    fix:  ALLOWED_HOSTS env typo

--- civic/settings.py
+++ civic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition

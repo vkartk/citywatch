caf8561b1b1c72838b01d8dbbc3d3d02453728a8
vkartk <53650724+vkartk@users.noreply.github.com>
2025-03-02 15:44:00 +0530

    chore: update ALLOWED_HOSTS

--- civic/settings.py
+++ civic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default=['*'])
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(",")
 
 
 # Application definition
